5b9f386d4719c9691c12e5713e98b417c85a0a62
Ilya Novozhilov <Ilya_Novozhilov@epam.com>
2016-06-27 15:05:51 +0300

    add functions of user

--- src/main/java/com/epam/spring/users/User.java
+++ src/main/java/com/epam/spring/users/User.java
@@ -1,5 +1,46 @@
 package com.epam.spring.users;
 
-public class User {
+import java.util.ArrayList;
+import java.util.List;
 
+public class User {
+	private int id;
+	private String name;
+	private String email;
+	private List<Ticket> tickets;
+	
+	public User(int id, String name, String email) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.tickets = new ArrayList<Ticket>();
+	}
+
+	public String getName() {
+		return name;
+	}
+    
+	public void addTicket(Ticket newTicket) {
+		this.tickets.add(newTicket);
+	}
+	
+	public void removeTicket(int index) {
+		this.tickets.remove(index);
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public int getId() {
+		return id;
+	}
 }
