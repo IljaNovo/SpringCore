fd187b6942c39b0954b6ad789be001fc5d7d20a8
Ilya Novozhilov <Ilya_Novozhilov@epam.com>
2016-06-27 15:07:20 +0300

    add functions of UserService

--- src/main/java/com/epam/spring/servises/UserService.java
+++ src/main/java/com/epam/spring/servises/UserService.java
@@ -1,26 +1,44 @@
 package com.epam.spring.servises;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.epam.spring.dao.DataBase;
 import com.epam.spring.users.User;
+import com.epam.spring.users.Event;
 
 public class UserService {
-	public void register() {
-		
+	
+	public void register(String name, String email) {
+		DataBase.set(name, email);
 	}
 	
-	public void remove() {
-		
+	public void remove(int index) {
+		DataBase.delete(index);
 	}
 	
-	public User getById() {
-		return new User();
+	public User getById(int index) {
+		return DataBase.getBase().get(index);
 	}
 	
-	public String getUserByEmail() {
-		return "";
+	public User getUserByEmail(String email) {
+		for (Integer key: DataBase.getBase().keySet()) {
+			if (DataBase.getBase().get(key).getEmail().equals(email)) {
+				return DataBase.getBase().get(key);
+			}
+		}
+		return null;
 	}
 	
-	public String getUsersByName() {
-		return "";
+	public List<User> getUsersByName(String name) {
+		List<User> usersNames = new ArrayList<User>();
+		for (Integer key: DataBase.getBase().keySet()) {
+			if (DataBase.getBase().get(key).getEmail().equals(name)) {
+				usersNames.add(DataBase.getBase().get(key));
+			}
+		}
+		return usersNames;
 	}
 	
 	public String getBookedTickets() {
