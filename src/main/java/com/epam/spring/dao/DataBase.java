dd7537e927c1fca3108fda4b417582871eb60a90
Ilya Novozhilov <Ilya_Novozhilov@epam.com>
2016-06-27 15:06:43 +0300

    add meta data base

--- src/main/java/com/epam/spring/dao/DataBase.java
+++ src/main/java/com/epam/spring/dao/DataBase.java
@@ -1,5 +1,35 @@
 package com.epam.spring.dao;
 
-public class DataBase {
+import java.util.HashMap;
+import java.util.Map;
 
+import com.epam.spring.users.User;
+
+public class DataBase {
+	private static Map<Integer, User> events = new HashMap<Integer, User>();
+	private static DataBase base;
+	private static int count = 0;
+	
+	private DataBase() {}
+	
+	public static DataBase instanse() {
+		if (base == null) {
+			base = new DataBase();
+		}
+		return base;
+	}
+	
+	public static void set(String name, String email) {
+		events.put(count, new User(count, name, email));
+		++count;
+	}
+	
+	public static void delete(int index) {
+		events.remove(index);
+		--count;
+	}
+	
+	public static Map<Integer, User> getBase() {
+		return events;
+	}
 }
