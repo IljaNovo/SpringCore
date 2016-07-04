cf0bbddb0da73b3ae378c9e7f409e6a86f0ebb60
Ilya Novozhilov <Ilya_Novozhilov@epam.com>
2016-07-04 17:10:05 +0300

    add new implement simpleIdGenerator

--- src/main/java/com/epam/spring/utils/SimpleIdGenerator.java
+++ src/main/java/com/epam/spring/utils/SimpleIdGenerator.java
@@ -1,5 +1,28 @@
 package com.epam.spring.utils;
 
-public class IdGenerater {
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SimpleIdGenerator implements IdGenerator{
+
+	private Map<DataType, Integer> idOfData;
 	
+	public SimpleIdGenerator(List<DataType> dataTypes) {
+		this.idOfData = new HashMap<DataType, Integer>();
+		for (int i = 0; i < dataTypes.size(); ++i) {
+			this.idOfData.put(dataTypes.get(i), 0);
+		}
+	}
+	
+	public int generate(DataType data) {
+	    this.idOfData.put(data, this.idOfData.get(data) + 1);
+	    return this.idOfData.get(data);
+	}
+
+	public void addDataType(DataType data) {
+		if (this.idOfData.get(data) != null) {
+			this.idOfData.put(data, 0);
+		}
+	}
 }
