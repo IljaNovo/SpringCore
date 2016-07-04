7a3e766c90eb3e5a0d79c6239d7404d0c686615a
Ilya Novozhilov <Ilya_Novozhilov@epam.com>
2016-07-04 16:50:26 +0300

    add constructor in simple counter

--- src/main/java/com/epam/spring/utils/SimpleCounter.java
+++ src/main/java/com/epam/spring/utils/SimpleCounter.java
@@ -1,5 +1,30 @@
 package com.epam.spring.utils;
 
-public class Counter {
+import java.util.Map;
 
+public class SimpleCounter implements Counter {
+	
+	private Map<DataType, Integer> countsOfData;
+	
+	public SimpleCounter(Map<DataType, Integer> countsOfData) {
+		this.countsOfData = countsOfData;
+	}
+	
+	public int getCount(DataType data) {
+		return this.countsOfData.get(data);
+	}
+	public void increase(DataType data) {
+		this.countsOfData.put(data, this.countsOfData.get(data) + 1);
+	}
+	public void reduce(DataType data) {
+		this.countsOfData.put(data, this.countsOfData.get(data) - 1);
+	}
+	public void reset(DataType data) {
+		this.countsOfData.put(data, 0);
+	}
+	public void addDataType(DataType data) {
+		if (this.countsOfData.get(data) != null) {
+			this.countsOfData.put(data, 0);
+		}
+	}
 }
