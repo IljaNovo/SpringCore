8c789406368cd51bc0c5e2e2b37c367fd2ca92f7
Ilya Novozhilov <Ilya_Novozhilov@epam.com>
2016-07-04 17:11:04 +0300

    add new implement simple counter

--- src/main/java/com/epam/spring/utils/SimpleCounter.java
+++ src/main/java/com/epam/spring/utils/SimpleCounter.java
@@ -1,27 +1,36 @@
 package com.epam.spring.utils;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class SimpleCounter implements Counter {
 	
 	private Map<DataType, Integer> countsOfData;
 	
-	public SimpleCounter(Map<DataType, Integer> countsOfData) {
-		this.countsOfData = countsOfData;
+	public SimpleCounter(List<DataType> dataTypes) {
+		this.countsOfData = new HashMap<DataType, Integer>();
+		for (int i = 0; i < dataTypes.size(); ++i) {
+			this.countsOfData.put(dataTypes.get(i), 0);
+		}
 	}
 	
 	public int getCount(DataType data) {
 		return this.countsOfData.get(data);
 	}
+	
 	public void increase(DataType data) {
 		this.countsOfData.put(data, this.countsOfData.get(data) + 1);
 	}
+	
 	public void reduce(DataType data) {
 		this.countsOfData.put(data, this.countsOfData.get(data) - 1);
 	}
+	
 	public void reset(DataType data) {
 		this.countsOfData.put(data, 0);
 	}
+	
 	public void addDataType(DataType data) {
 		if (this.countsOfData.get(data) != null) {
 			this.countsOfData.put(data, 0);
