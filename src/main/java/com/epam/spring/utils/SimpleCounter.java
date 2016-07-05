2481f5adcedae694dd92d35999de9fbc0b275013
Ilya Novozhilov <ilja.keinn@mail.ru>
2016-07-05 05:07:08 +0300

    add additional utils - counter and id generater

--- src/main/java/com/epam/spring/utils/SimpleCounter.java
+++ src/main/java/com/epam/spring/utils/SimpleCounter.java
@@ -4,45 +4,45 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class SimpleCounter{
-	
+public class SimpleCounter {
+
 	private static Map<DataType, Integer> countsOfData;
 	private static SimpleCounter elem;
-	
+
 	private SimpleCounter() {
 	}
-	
-	public static void addListDataType(List<DataType> dataTypes) {
+
+	public void addListDataType(List<DataType> dataTypes) {
 		countsOfData = new HashMap<DataType, Integer>();
 		for (int i = 0; i < dataTypes.size(); ++i) {
 			countsOfData.put(dataTypes.get(i), 0);
 		}
 	}
-	
+
 	public static SimpleCounter instanse() {
 		if (elem == null) {
 			elem = new SimpleCounter();
 		}
 		return elem;
 	}
-	
-	public static int getCount(DataType data) {
+
+	public int getCount(DataType data) {
 		return countsOfData.get(data);
 	}
-	
-	public static void increase(DataType data) {
+
+	public void increase(DataType data) {
 		countsOfData.put(data, countsOfData.get(data) + 1);
 	}
-	
-	public static void reduce(DataType data) {
+
+	public void reduce(DataType data) {
 		countsOfData.put(data, countsOfData.get(data) - 1);
 	}
-	
-	public static void reset(DataType data) {
+
+	public void reset(DataType data) {
 		countsOfData.put(data, 0);
 	}
-	
-	public static void addDataType(DataType data) {
+
+	public void addDataType(DataType data) {
 		if (countsOfData.get(data) != null) {
 			countsOfData.put(data, 0);
 		}
--- src/main/java/com/epam/spring/utils/SimpleIdGenerator.java
+++ src/main/java/com/epam/spring/utils/SimpleIdGenerator.java
@@ -4,34 +4,34 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class SimpleIdGenerator{
+public class SimpleIdGenerator {
 
 	private static Map<DataType, Integer> idOfData = new HashMap<DataType, Integer>();
 	private static SimpleIdGenerator elem;
-	
+
 	private SimpleIdGenerator() {
 	}
-	
-	public static SimpleIdGenerator instance() {
+
+	public static SimpleIdGenerator instanse() {
 		if (elem == null) {
 			elem = new SimpleIdGenerator();
 		}
 		return elem;
 	}
-	
-	public static void addListDataTypes(List<DataType> dataTypes) {
+
+	public void addListDataTypes(List<DataType> dataTypes) {
 		idOfData = new HashMap<DataType, Integer>();
 		for (int i = 0; i < dataTypes.size(); ++i) {
 			idOfData.put(dataTypes.get(i), 0);
 		}
 	}
-	
-	public static int generate(DataType data) {
-	    idOfData.put(data, idOfData.get(data) + 1);
-	    return idOfData.get(data);
+
+	public int generate(DataType data) {
+		idOfData.put(data, idOfData.get(data) + 1);
+		return idOfData.get(data);
 	}
 
-	public static void addDataType(DataType data) {
+	public void addDataType(DataType data) {
 		if (idOfData.get(data) != null) {
 			idOfData.put(data, 0);
 		}
