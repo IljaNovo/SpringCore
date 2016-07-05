40960f7ffd7eb66460e44fc308fdf4abdced18c5
Ilya Novozhilov <ilja.keinn@mail.ru>
2016-07-05 06:24:02 +0300

    add new methods and constructors in customer date

--- src/main/java/com/epam/spring/movie/date/CustomerDate.java
+++ src/main/java/com/epam/spring/movie/date/CustomerDate.java
@@ -6,44 +6,56 @@ import java.util.Calendar;
 import java.util.GregorianCalendar;
 
 public class CustomerDate {
-	private Calendar timeStartMovie;
+	private Calendar time;
+	
+	public CustomerDate() {
+		this.time = new GregorianCalendar();
+	}
+
+	public CustomerDate(int year, int month, int dayOfMonth) {
+		this.time = new GregorianCalendar(year, month, dayOfMonth);
+	}
 	
 	public CustomerDate(int year, int month, int dayOfMonth, int hourOfDay, int minute, int second) {
-		this.timeStartMovie = new GregorianCalendar(year, month, dayOfMonth, hourOfDay, minute, second);
+		this.time = new GregorianCalendar(year, month, dayOfMonth, hourOfDay, minute, second);
 	}
 	
 	public String getFullDate() {
 		DateFormat df = new SimpleDateFormat("yyyy MM dd");
-		return df.format(timeStartMovie.getTime());
+		return df.format(time.getTime());
 	}
 	
 	public String getYear() {
 		DateFormat df = new SimpleDateFormat("yyyy");
-		return df.format(timeStartMovie.getTime());
+		return df.format(time.getTime());
 	}
 
 	public String getMonth() {
 		DateFormat df = new SimpleDateFormat("MM");
-		return df.format(timeStartMovie.getTime());
+		return df.format(time.getTime());
 	}
 
 	public String getDay() {
 		DateFormat df = new SimpleDateFormat("dd");
-		return df.format(timeStartMovie.getTime());
+		return df.format(time.getTime());
 	}
 
 	public String getHours() {
 		DateFormat df = new SimpleDateFormat("HH");
-		return df.format(timeStartMovie.getTime());
+		return df.format(time.getTime());
 	}
 
 	public String getMinutes() {
 		DateFormat df = new SimpleDateFormat("mm");
-		return df.format(timeStartMovie.getTime());
+		return df.format(time.getTime());
 	}
 
 	public String getSecond() {
 		DateFormat df = new SimpleDateFormat("ss");
-		return df.format(timeStartMovie.getTime());
+		return df.format(time.getTime());
+	}
+	
+	public long getTimeInMillis() {
+		return this.time.getTimeInMillis();
 	}
 }
