69342a9249d1fd755fb0f366ac195281afbadf97
Ilya Novozhilov <ilja.keinn@mail.ru>
2016-07-08 11:04:37 +0300

    injected Dao in CounterAspect

--- src/main/java/com/epam/spring/aspects/CounterAspect.java
+++ src/main/java/com/epam/spring/aspects/CounterAspect.java
@@ -1,9 +1,21 @@
 package com.epam.spring.aspects;
 
+import com.epam.spring.dao.interfaces.CounterByEventNameDao;
+import com.epam.spring.dao.interfaces.CounterByPriceDao;
+import com.epam.spring.dao.interfaces.CounterByTicketDao;
+
 public class CounterAspect {
-	private String base;
+	private CounterByEventNameDao ByEventNameBase;
+	private CounterByPriceDao ByPriseBase;
+	private CounterByTicketDao ByTicketBase;
 	
-	public CounterAspect() {
-		this.base = base;
+	public CounterAspect(CounterByEventNameDao byEventNameBase,
+						 CounterByPriceDao byPriseBase,
+						 CounterByTicketDao byTicketBase) {
+		ByEventNameBase = byEventNameBase;
+		ByPriseBase = byPriseBase;
+		ByTicketBase = byTicketBase;
 	}
+	
+	
 }
