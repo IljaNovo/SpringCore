fe0f9f7add1f5d81fae256b9f344e8641c2f12db
Ilya Novozhilov <Ilya_Novozhilov@epam.com>
2016-07-08 18:03:55 +0300

    fixed code structure of Demo

--- src/main/java/com/epam/spring/app/Demo.java
+++ src/main/java/com/epam/spring/app/Demo.java
@@ -19,7 +19,6 @@ import com.epam.spring.servises.BookingService;
 import com.epam.spring.servises.DiscountService;
 import com.epam.spring.servises.EventService;
 
-
 // Небольшое демо, здесь из контейнера берётся user, event
 // Создаётся сессия (сеанс для фильма)
 // Создаётся тикет
@@ -32,36 +31,37 @@ import com.epam.spring.servises.EventService;
 public class Demo {
 	public static void main(String[] args) {
 		ApplicationContext actx = new ClassPathXmlApplicationContext("all-beans.xml");
-		
+
 		DiscountService disServise = (DiscountService) actx.getBean("discount-servise");
 		AuditoriumService room = (AuditoriumService) actx.getBean("auditorium-servise");
 		BookingService bookServece = (BookingService) actx.getBean("booking-servise");
-		
+
 		User customer = (User) actx.getBean("user2");
-		Event film = (Event)actx.getBean("event1");
-		Event film2 = (Event)actx.getBean("event2");
+		Event film = (Event) actx.getBean("event1");
+		Event film2 = (Event) actx.getBean("event2");
 		Session ses = new Session(film, room.getAuditoriums().get(0), new CustomerDate(2000, 07, 13));
-		
+
 		double price = film.getPrice() * disServise.getDiscount(customer, film, new CustomerDate(1995, 04, 20));
 		Ticket order = new Ticket(price, "1", ses);
-		
+
 		bookServece.bookTicket(customer, order);
-		
+
 		System.out.print("Количество заказов на " + new CustomerDate(2000, 01, 13).getFullDate() + " : ");
 		System.out.println(bookServece.getTicketsForEvent(film, new CustomerDate(2000, 07, 13)).size());
-        MapEventDao med = (MapEventDao) actx.getBean("map_event_dao");
-        med.setEvent(film);
-        med.setEvent(film2);
-        
-        EventService es = (EventService) actx.getBean("event-servise");
-        es.getEventByName("eventName1");
-        es.getEventByName("eventName1");
-        es.getEventByName("eventName1");
-        
-        MapCounterByEventNameDao mcbend = (MapCounterByEventNameDao)actx.getBean("map_counter_by_event_name_dao");
-        System.out.println("Количество обращений к фильму " + film.getName() + ": " + mcbend.getCount(film));
-        
-        MapDiscountDao mdd = (MapDiscountDao) actx.getBean("map_discount_dao");
-        System.out.println("Общая скидка для пользователя " + customer.getName() + ": " + mdd.getSpecificDiscount(customer));
+		MapEventDao med = (MapEventDao) actx.getBean("map_event_dao");
+		med.setEvent(film);
+		med.setEvent(film2);
+
+		EventService es = (EventService) actx.getBean("event-servise");
+		es.getEventByName("eventName1");
+		es.getEventByName("eventName1");
+		es.getEventByName("eventName1");
+
+		MapCounterByEventNameDao mcbend = (MapCounterByEventNameDao) actx.getBean("map_counter_by_event_name_dao");
+		System.out.println("Количество обращений к фильму " + film.getName() + ": " + mcbend.getCount(film));
+
+		MapDiscountDao mdd = (MapDiscountDao) actx.getBean("map_discount_dao");
+		System.out.println(
+				"Общая скидка для пользователя " + customer.getName() + ": " + mdd.getSpecificDiscount(customer));
 	}
 }
